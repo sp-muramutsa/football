4862b51a67219ae7cbab969a7aca6fdd14858add
sp-muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-06-28 08:34:50 -0500

    Refactored the project to only consider attackers

--- football.py
+++ football.py
@@ -3,37 +3,68 @@ from sklearn.preprocessing import MinMaxScaler, normalize
 import pandas as pd
 import streamlit as st
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_excel("top5-players24-25.xlsx") 
+df = pd.read_excel("data/top5-players24-25.xlsx") 
 
-X = df[['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt', 'CrdY','CrdR',
-        'xG', 'npxG', 'xAG', 'npxG+xAG', 'PrgC', 'PrgP', 'PrgR']]
+# Only keep Forwards
+df = df[df["Pos"].str.contains("FW")]
+
+X = df[['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt', 'xG',
+        'npxG', 'xAG', 'npxG+xAG', 'PrgC', 'PrgP', 'PrgR']]
+
 
 scaler = MinMaxScaler()
-X_rescaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X)
 
-nmf = NMF(n_components=5)
-nmf_features = nmf.fit_transform(X_rescaled)
-norm_features = normalize(nmf_features)
+nmf = NMF(max_iter=500)
+nmf_features = nmf.fit_transform(X_scaled)
+normalized_features = normalize(nmf_features)
 
 players = df["Player"]
 
-def get_similar_players(target_player, norm_features, players, top_n=10):
+def get_similar_players(target_player, normalized_features, players, top_n=10):
     try:
         idx = np.where(players == target_player)[0][0]
     except IndexError:
         return []
-    player_vec = norm_features[idx].reshape(1, -1)
-    similarities = norm_features.dot(player_vec.T).flatten()
-    similar_idx = similarities.argsort()[::-1][1:top_n+1]  # skip self
-    return [(players[i], similarities[i]) for i in similar_idx]
+    
+    target_player_features_vector = normalized_features[idx]
+    similarities = normalized_features.dot(target_player_features_vector)
+    similar_players_indices = similarities.argsort()[::-1][1:top_n+1] # skip self
+    
+    return [(players.iloc[i], similarities[i]) for i in similar_players_indices]
 
 st.title("Attacking Productivity Similarity Recommender (Europe Top 5 Domestic Leagues: 24/25)")
 
-selected_player = st.selectbox("Select a player:", players)
+selected_player = st.selectbox("Select a player:", players, index=979)
 
 if selected_player:
-    recommendations = get_similar_players(selected_player, norm_features, players, top_n=10)
-    st.write(f"Top 10 players similar to **{selected_player}**:")
+    recommendations = get_similar_players(selected_player, normalized_features, players, top_n=5)
+    st.write(f"Top 5 Attackers with similar productivity to **{selected_player}'s**:")
     for name, score in recommendations:
         st.write(f"- {name} (Similarity: {score:.3f})")
+   
+    recommendations_df = pd.DataFrame({
+        "name": name,
+        "score": score
+        }
+        for name, score in recommendations
+    )
+
+    fig, ax = plt.subplots()
+    ax = sns.barplot(data=recommendations_df, x="score", y="name", palette="rocket", orient="h")
+    ax.set_title(f"Top 5 Attackers with similar productivity to {selected_player}'s.")
+    st.pyplot(fig)
+
+st.markdown("---")
+st.markdown("""
+**About this model**  
+This model employs **Non-negative Matrix Factorization (NMF)** to recommend attackers with similar productivity profiles from the top five European domestic leagues (Premier League, La Liga, Bundesliga, Serie A, and Ligue 1) for the 2024/25 season.  
+
+It uses the following performance metrics:  
+`['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt', 'xG', 'npxG', 'xAG', 'npxG+xAG', 'PrgC', 'PrgP', 'PrgR']`
+
+The goal is to help teams find replicate an attacker's final product **without being being necessarily concerned about positions and/or play styles**.
+""")
